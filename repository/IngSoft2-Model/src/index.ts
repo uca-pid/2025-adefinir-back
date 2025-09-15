23e22318feccf9f7559591db655a13ad0296c789
Belen-Eliza <beluella1@gmail.com>
2025-09-15 12:11:28 -0300

    server básico

--- repository/IngSoft2-Model/src/index.ts
+++ repository/IngSoft2-Model/src/index.ts
@@ -1,21 +1,39 @@
 const { PrismaClient } = require('@prisma/client');
+const express  = require('express')
+import type { NextFunction, Request, Response } from 'express'
 
+// correr con npm start
+
+const app = express()
+const port = 3000
 const prisma = new PrismaClient();
 
-async function main() {
-  //change to reference a table in your schema
+app.use(express.json());
+// addRoutes(app, prisma)
+
+app.get('/', async (req: Request, res: Response) => {
+  res.send('Hello World!')
+
   const val = await prisma.Users.findMany({
     take: 10,
   });
   console.log(val);
+
+})
+
+function logErrors(err: Error, req: Request, res: Response, next: NextFunction) {
+  console.error(err.stack);
+  next(err);
 }
 
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-  process.exit(1);
-});
+app.use(logErrors)
+
+function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
+  res.status(500).send({ errors: [{ message: "Something went wrong" }] });
+};
+
+app.use(logErrors)
+app.use(errorHandler)
+
+const server = app.listen(port, () =>
+  console.log(`🚀 Server ready on port ${port}`))
